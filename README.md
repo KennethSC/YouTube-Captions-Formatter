9809c08f2a488e33ec3d6b6e0682a6d046b7baa2
KennethSC <kennethsantacruz12@gmail.com>
2020-07-04 13:14:17 -0700

    Small fixes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Desription
 
-This script uses the YouTube API to get the captions transcript for a video, creates a file in the users Documents folder called 'Lecture_Today's Date', and writes the captions of the desired video to that file in a formatted way. Since classes are now online, some of my professors have been uploading their lectures to YouTube. Now with the help of this script I can take notes during class, but also have notes of everything that was said in that lecture, making it easier and quicker to access/refer to specific information from that lecture by just doing a keyword search on the file containing the captions.
+This script uses the YouTube API to get the captions transcript for a video, creates a file in the users Documents folder called "Lecture_Today's Date'", and writes the captions of the desired video to that file in a formatted way. Since classes are now online, some of my professors have been uploading their lectures to YouTube. Now with the help of this script I can take notes during class, but also have notes of everything that was said in that lecture, making it easier and quicker to access/refer to specific information from that lecture by just doing a keyword search on the file containing the captions.
 
 ## Requirements
 
@@ -11,9 +11,7 @@ To install this API (for Mac users) use this command in your terminal: pip insta
 
 ## Instructions
 
-Copy the URL of your desired YouTube video and paste it into the script so that it is set equal to the variable called 'URL'.
-
-EX: URL = 'YourVideoURL'
+Just copy the URL of your desired YouTube video, run the script, and paste the URL into the command line when prompted.
 
 
-To access the created file, just go to your 'Documents' folder and open the file.
+To access the created file, just go to your 'Documents' folder and open the file titled "Lecture_Today's Date.txt" 
--- YouTube_Captions.py
+++ YouTube_Captions.py
@@ -1,40 +1,59 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from datetime import date
+import sys
 import os
 
-# Copy the URL of your desired YouTube video and set the URL
-# variable below equal to the copied URL
-# EX: URL = 'ThisIsYourVideosURL'
 
-# Your YouTube video URL goes here:
-URL = 'https://www.youtube.com/watch?v=v1I3inlM35k'
+# Copy the URL of your desired YouTube video
+# and paste it into the command line when prompted
+
+# Takes in the video URL as input
+URL = input("Paste the video URL here: ")
 
 split_at = 'watch?v='
 
-# Extracts the video ID from the URL
-video_id = URL.partition(split_at)[2] 
+# Extracts the video ID from the URL,
+# throws an error if it is an invalid
+# YouTube URL
+if split_at in URL:
+    video_id = URL.partition(split_at)[2]
+else:
+    print("ERROR: This is not a valid YouTube URL")
+    sys.exit(0)
+
 
 # Uses a given function from the YouTube API to get
-# the captions transcript of desired video
-trans_list = YouTubeTranscriptApi.get_transcript(video_id)
+# the captions transcript of the desired video,
+# throws an error if the video doesn't have a captions transcript
+try:
+    trans_list = YouTubeTranscriptApi.get_transcript(video_id)
+except:
+    print("ERROR: This video doesn't have captions")
+    sys.exit(0)
+
 
 # Takes the captions form the transcript and puts them into a list
 captions = [ sub['text'] for sub in trans_list ]
+
 
 # Gets the current date and formats it
 today = date.today()
 date = today.strftime("%b-%d-%Y")
 
-# Set the defualt file name to
+
+# Sets the defualt file name to
 # 'Lecture_Today's Date.txt'
 fileName = "Lecture_" + str(date) + ".txt"
+
 
 # Joins a path to the users Documents folder
 # to store the file in that folder
 path_to_Docs = os.path.join(os.path.expanduser('~'), 'Documents', fileName)
 
+
 # Creates and opens the file for writing
-f = open(path_to_Docs,"w+")
+f = open(path_to_Docs, "w+")
+
 
 # Counter that helps with formatting
 lineCounter = 0
@@ -45,7 +64,7 @@ for caption in captions:
     lineCounter += 1
     f.write(str(caption) + ' ')
     
-    if lineCounter == 10:
+    if lineCounter == 8:
         f.write("\n")
         lineCounter = 0
 
@@ -54,4 +73,5 @@ for caption in captions:
 f.close() 
 
 
+        
         
