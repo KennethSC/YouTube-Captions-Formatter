c4c91af43427f18f3159d77ccdea756bdcdfea34
KennethSC <kennethsantacruz12@gmail.com>
2020-07-07 19:59:43 -0700

    Final Commit

--- YouTube_Captions.py
+++ YouTube_Captions.py
@@ -3,84 +3,114 @@ import sys
 import os
 
 
-# Copy the URL of your desired YouTube video
-# and paste it into the command line when prompted
+# Takes in the video URL as input and
+# extracts the video ID from the URL.
+# Throws an error if it is an invalid
+# YouTube URL, otherwise returns the video ID
+def get_URL():
 
-# Takes in the video URL as input
-URL = input("Enter your video's URL: ")
+    URL = input("Enter your video's URL: ")
+
+    # Every valid YouTube video URL should
+    # contain this line. The video ID comes
+    # after this line for every URL
+    split_at = 'watch?v='
 
 
-# Every valid YouTube video URL should
-# contain this line. The video ID comes
-# after this line for every URL
-split_at = 'watch?v='
+    if 'https://www.youtube.com/watch?v=' in URL:
+        video_id = URL.partition(split_at)[2]
+    else:
+        print("ERROR: This is not a valid YouTube URL.")
+        sys.exit(0)
+        
+        
+    return video_id
 
-
-# Extracts the video ID from the URL,
-# throws an error if it is an invalid
-# YouTube URL
-if 'https://www.youtube.com/watch?v=' in URL:
-    video_id = URL.partition(split_at)[2]
-else:
-    print("ERROR: This is not a valid YouTube URL.")
-    sys.exit(0)
 
 
 # Uses a given function from the YouTube API to get
-# the captions transcript of the desired video,
-# throws an error if the video doesn't have a captions transcript
-try:
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
-except:
-    print("ERROR: This video doesn't have captions.")
-    sys.exit(0)
+# the captions transcript of the desired video.
+# Throws an error if the video doesn't have a captions transcript,
+# otherwise returns the captions as a list of words.
+def get_transcript(vid_ID):
+
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(vid_ID)
+    except:
+        print("ERROR: This video doesn't have captions.")
+        sys.exit(0)
 
 
-# Takes the captions from the transcript and puts them into a list
-# making a list of sentences.
-captions_list = [ sub['text'] for sub in transcript ]
+    # Takes the captions from the transcript and puts them into a list
+    # making a list of sentences.
+    captions_list = [ sub['text'] for sub in transcript ]
 
 
-# Takes the captions_list above and turns it from a list of 
-# sentences to a list of words.
-captions = [word for line in captions_list for word in line.split()]
+    # Takes the captions_list above and turns it from a list of 
+    # sentences to a list of words.
+    captions = [word for line in captions_list for word in line.split()]
+
+    return captions
 
 
-# Asks the user what they want the file to be named
-file = input("\nWhat do you want the file to be called?: ")
+
+# Asks the user what they want the file to be named,
+# makes the file a .txt file, and joins a path to the user's 
+# 'Documents' folder to store the file in that folder.
+# Returns the file with specified name
+def make_file():
+
+    file = input("\nWhat do you want the file to be called?: ")
+
+    if ".txt" not in file:
+        completeName = str(file) + ".txt"
+    else:
+        completeName = str(file)
 
 
-# Makes the file a .txt file
-if ".txt" not in file:
-    completeName = str(file) + ".txt"
-else:
-    completeName = str(file)
+    path_to_Docs = os.path.join(os.path.expanduser('~'), 'Documents', completeName)
+
+    return path_to_Docs
 
 
-# Joins a path to the user's Documents folder
-# to store the file in that folder
-path_to_Docs = os.path.join(os.path.expanduser('~'), 'Documents', completeName)
 
-
-# Creates and opens the file for writing
-f = open(path_to_Docs, "w+")
-
-
-# Counter that helps with formatting
-wordCounter = 0
-
-# Writes the captions in a formatted
-# way to the file
-for caption in captions:
-    wordCounter += 1
-
-    f.write(str(caption) + ' ')
+# Creates and opens the file for writing,
+# writes the captions in a formatted way to 
+# the file, and then closes the file.
+def format_file(file, captions):
     
-    if wordCounter == 11:
-        f.write("\n")
-        wordCounter = 0
+    f = open(file, "w+")
+
+    # Counter that helps with formatting
+    wordCounter = 0
+
+    for caption in captions:
+        wordCounter += 1
+
+        f.write(str(caption) + ' ')
+        
+        if wordCounter == 11:
+            f.write("\n")
+            wordCounter = 0
+
+    f.close()
 
 
-# Closes the file
-f.close() 
+
+# Main driver function that calls
+# all the other functions
+def main():
+
+    video_ID = get_URL()
+    captions = get_transcript(video_ID)
+    file = make_file()
+    format_file(file, captions)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
 
